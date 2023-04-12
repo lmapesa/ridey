05150d50c3e396621da5ccfa45444c5fedd943b1
lmapesa <levinmapesa@gmail.com>
2023-04-12 11:20:27 +0300

    tests

--- dbt/models/intermediate/int_payment.sql
+++ dbt/models/intermediate/int_payment.sql
@@ -5,7 +5,7 @@ WITH payment as (
 
 ),
 
-final as (
+stage as (
     SELECT 
         trip_id,
         taxi_id,
@@ -14,17 +14,27 @@ final as (
         tolls,
         extras,
         SUM(fare + tips + tolls + extras) AS trip_total,
-        -- SUM(fare + tips + tolls + extras) OVER (PARTITION BY trip_id) AS trip_total,
-        
-        {% for payment_method in payment_methods -%}
-            sum(case when payment_type = '{{ payment_type }}' then trip_total else 0 end) as {{ payment_type }}_trip_total,
-        {% endfor -%}
-        
-        payment_type
-
+        COALESCE(payment_type, 'Unknown') AS payment_type
 
     FROM payment
     GROUP BY trip_id, taxi_id, fare, tips, tolls, extras, payment_type
+),
+
+final as (
+    SELECT 
+        trip_id,
+        taxi_id,
+        fare,
+        tips,
+        tolls,
+        extras,
+        {% for payment_method in payment_methods -%}
+            sum(case when payment_type = '{{ payment_type }}' then trip_total else 0 end) as {{ payment_type }}_trip_total,
+        {% endfor -%}
+
+        payment_type
+    FROM stage
+
 )
 
 SELECT * FROM final
